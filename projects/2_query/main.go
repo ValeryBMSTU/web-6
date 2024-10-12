0528218d5e4698c4d384d37728acf04eebf7fba4
ZeusKronosovich <Korotaev6776@mail.com>
2024-10-12 13:45:52 +0300

    1

--- projects/2_query/main.go
+++ projects/2_query/main.go
@@ -1,9 +1,19 @@
 package main
 
-// здесь надо написать код
+import (
+	"net/http"
+)
 
 func main() {
-	// и здесь тоже
-}
 
-// и тут тоже (если очень надо)
+	http.HandleFunc("/api/user", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+
+		w.Write([]byte("Hello,"))
+		w.Write([]byte(name))
+		w.Write([]byte("!"))
+	})
+
+	http.ListenAndServe(":9000", nil)
+
+}
